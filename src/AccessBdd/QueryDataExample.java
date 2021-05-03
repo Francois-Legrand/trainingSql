b1c7aefa14c2f235bec5ba97768ddd0d8aa60991
Développeur web <51367556+Francois-Legrand@users.noreply.github.com>
2021-05-03 13:16:08 +0200

    update

--- src/AccessBdd/QueryDataExample.java
+++ src/AccessBdd/QueryDataExample.java
@@ -64,101 +64,10 @@ public class QueryDataExample {
 					}
 					System.out.println("Saisissez l'identifiant de l'acteur pour avoir plus d'information");
 					choix = scanner.nextLine();
-
-					Acteur acteur = acteurService.findById(Integer.parseInt(choix));
-					try {
-
-						Acteur acteurId = new Acteur();
-
-						Connection connection = ConnectionUtils.getMyConnection();
-
-						Statement statement = connection.createStatement();
-
-						String sql = "SELECT actor.actor_id, actor.first_name, actor.last_name,actor.last_update FROM actor "
-								+ "WHERE actor.actor_id =" + Integer.parseInt(choix) + ";";
-
-						ResultSet rs = statement.executeQuery(sql);
-
-						while (rs.next()) {
-
-							int acteur_id = rs.getInt("actor.actor_id");
-							System.out.print(acteur_id + "/");
-							acteurId.setActor_id(acteur_id);
-
-							String first_name = rs.getString("actor.first_name");
-							acteurId.setFirst_name(first_name);
-							System.out.print(first_name + " ");
-
-							String last_name = rs.getString("actor.last_name");
-							acteurId.setLast_name(last_name);
-							System.out.println(last_name);
-
-							Timestamp last_update = rs.getTimestamp("actor.last_update");
-							acteurId.setLast_update(last_update);
-							System.out.println("Mise à jour: " + last_update);
-							acteurId.ajouterFilm(film);
-						}
-						rs.close();
-
-						int page = 0;
-						boolean sortir = false;
-						while (!sortir) {
-							String sql2 = "SELECT * FROM category JOIN film_category ON category.category_id = film_category.category_id "
-									+ "JOIN film ON film_category.film_id = film.film_id JOIN film_actor ON film.film_id = film_actor.film_id "
-									+ "JOIN actor ON film_actor.actor_id = actor.actor_id JOIN language ON film.language_id = language.language_id "
-									+ "WHERE film_actor.actor_id ="
-									+ Integer.parseInt(choix) + " limit " + page + ", 5";
-
-							ResultSet rs2 = statement.executeQuery(sql2);
-							System.out.println("Liste des films:");
-
-							while (rs2.next()) {
-								System.out.println("----------------------------\n");
-								int film_id = rs2.getInt("film.film_id");
-								film.setFilm_id(film_id);
-								System.out.print(film_id + "/");
-
-								String film_title = rs2.getString("film.title");
-								film.setTitle(film_title);
-								System.out.println(film_title);
-
-								String film_langage = rs2.getString("language.name");
-								film.setLanguage(film_langage);
-								System.out.println(film_langage);
-
-								String film_description = rs2.getString("film.description");
-								film.setDescription(film_description);
-								System.out.println("Description: " + film_description);
-
-								String category = rs2.getString("category.name");
-								film.setFilm_category(category);
-								System.out.println("Catégories: " + category);
-
-								Timestamp release_year = rs2.getTimestamp("film.release_year");
-								film.setRelease_year(release_year);
-								System.out.println("Sortie: " + release_year);
-
-							}
-							System.out.println("prev next ou sortir");
-							String choix2 = scanner.nextLine();
-							if (choix2.equals("next")) {
-								page += 5;
-							} else if (choix2.equals("prev")) {
-								page -= 5;
-							} else if (choix2.equals("sortir")) {
-								sortir = true;
-								returnStart = false;
-							}
-							rs2.close();
-
-						}
-					} catch (ClassNotFoundException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+					
+					pageable(choix);
+					returnStart = false;
+					
 				}
 				break;
 			case 3:
@@ -179,15 +88,110 @@ public class QueryDataExample {
 				String choixActeur = scanner.nextLine();
 				Acteur acteur = acteurService.findByName(choixActeur);
 				System.out.println("---------------------------------");
-				System.out.println(acteur.getFirst_name() + " " + acteur.getLast_name());
+				pageable(Integer.toString(acteur.getActor_id()));
 			default:
 				break;
 			}
 
 		}
-
+		scanner.close();
 	}
 	public static void pageable(String choix) {
+		
+		try {
+			Scanner scanner = new Scanner(System.in);
+			Film film = new Film();
+			Acteur acteurId = new Acteur();
+
+			Connection connection = ConnectionUtils.getMyConnection();
+
+			Statement statement = connection.createStatement();
+
+			String sql = "SELECT actor.actor_id, actor.first_name, actor.last_name,actor.last_update FROM actor "
+					+ "WHERE actor.actor_id =" + Integer.parseInt(choix) + ";";
+
+			ResultSet rs = statement.executeQuery(sql);
+
+			while (rs.next()) {
+
+				int acteur_id = rs.getInt("actor.actor_id");
+				System.out.print(acteur_id + "/");
+				acteurId.setActor_id(acteur_id);
+
+				String first_name = rs.getString("actor.first_name");
+				acteurId.setFirst_name(first_name);
+				System.out.print(first_name + " ");
+
+				String last_name = rs.getString("actor.last_name");
+				acteurId.setLast_name(last_name);
+				System.out.println(last_name);
+
+				Timestamp last_update = rs.getTimestamp("actor.last_update");
+				acteurId.setLast_update(last_update);
+				System.out.println("Mise à jour: " + last_update);
+				acteurId.ajouterFilm(film);
+			}
+			rs.close();
+			
+			int page = 0;
+			boolean sortir = false;
+			while (!sortir) {
+				String sql2 = "SELECT * FROM category JOIN film_category ON category.category_id = film_category.category_id "
+						+ "JOIN film ON film_category.film_id = film.film_id JOIN film_actor ON film.film_id = film_actor.film_id "
+						+ "JOIN actor ON film_actor.actor_id = actor.actor_id JOIN language ON film.language_id = language.language_id "
+						+ "WHERE film_actor.actor_id ="
+						+ Integer.parseInt(choix) + " limit " + page + ", 5";
+
+				ResultSet rs2 = statement.executeQuery(sql2);
+				System.out.println("Liste des films:");
+
+				while (rs2.next()) {
+					System.out.println("----------------------------\n");
+					int film_id = rs2.getInt("film.film_id");
+					film.setFilm_id(film_id);
+					System.out.print(film_id + "/");
+
+					String film_title = rs2.getString("film.title");
+					film.setTitle(film_title);
+					System.out.println(film_title);
+
+					String film_langage = rs2.getString("language.name");
+					film.setLanguage(film_langage);
+					System.out.println(film_langage);
+
+					String film_description = rs2.getString("film.description");
+					film.setDescription(film_description);
+					System.out.println("Description: " + film_description);
+
+					String category = rs2.getString("category.name");
+					film.setFilm_category(category);
+					System.out.println("Catégories: " + category);
+
+					Timestamp release_year = rs2.getTimestamp("film.release_year");
+					film.setRelease_year(release_year);
+					System.out.println("Sortie: " + release_year);
+
+				}
+				System.out.println("prev next ou sortir");
+				String choix2 = scanner.nextLine();
+				if (choix2.equals("next")) {
+					page += 5;
+				} else if (choix2.equals("prev")) {
+					page -= 5;
+				} else if (choix2.equals("sortir")) {
+					sortir = true;
+				}
+				rs2.close();
+				
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
 
